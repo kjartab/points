10e22b019b96b5646818d1fe4ca65244436cec98
Kjartan Bjørset <kjartanbjorset@gmail.com>
2015-05-05 00:03:15 +0200

    multithreading

---
+++ python/loadlidar_parallell.py
@@ -0,0 +1,40 @@
+from multiprocessing import Pool
+from subprocess import call
+import os
+import datetime
+
+rootdir = '/vagrant/las-data-xml-test'
+ 
+filesToLoad = []
+loaded = 0
+
+startScript = datetime.datetime.now()
+print startScript.strftime("%d.%m.%y_%H:%M:%S") + ' Start loading \n'
+    
+
+for root, dirs, filenames in os.walk(rootdir):
+    for file in filenames:
+        filesToLoad.append(rootdir +'/'+file)
+
+print "Files to load: " + str(len(filesToLoad)) + "\n";
+    
+results = 0
+
+def pdal_pipeline(file_path):
+    os.system("pdal pipeline " + file_path)
+    print "loaded one file nr: " + file_path
+    
+    return 1
+    
+pool = Pool(16)
+results = pool.map(pdal_pipeline, filesToLoad)
+    
+
+stopScript = datetime.datetime.now()
+
+
+print stopScript.strftime("%d.%m.%y_%H:%M:%S") + ' Stop loading \n'
+
+
+
+print 'Total run time: ' + str((stopScript - startScript).total_seconds())
